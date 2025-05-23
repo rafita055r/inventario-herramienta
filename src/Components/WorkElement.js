8405bfc6fce8bd38f9def00e74bb86a1c5e61f0b
EzeGomez0407 <gomezmerloe@gmail.com>
2025-05-23 10:17:50 -0300

    agregue btns para editar o eliminar obra, todavia no funcionan

--- src/Components/WorkElement.js
+++ src/Components/WorkElement.js
@@ -7,14 +7,20 @@ import { useState } from "react";
 export default function WorkElement({work}){
   const {getHerramienta} = useStore()
   const [isOpen, setIsOpen] = useState(false);
-    
+  
+  const openToolList = (e)=>{
+    setIsOpen(!isOpen)
+  }
   return(
     <li className='li_Work'>
-      <h3 onClick={() => setIsOpen(!isOpen)} style={{ cursor: "pointer" }}>
+      <h3 className={isOpen ? 'listToolOpen' : null} onClick={openToolList} style={{ cursor: "pointer" }}>
         {work.nombre}
       </h3>
       <div className={`tools-container ${isOpen ? "open" : ""}`}>
-
+        <div className='contain_btnDel_btnUpd'> 
+          <button className='btn_editWork'>Editar</button>
+          <button className='btn_delWork'>Eliminar</button>
+        </div>
         <ul className="listTools">
           {
             work.herramientas_enObra.length > 0 ? work.herramientas_enObra.map((tool) => {
