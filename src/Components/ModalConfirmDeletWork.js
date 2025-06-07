0984aca0829558f01325086d13ba4a0532176a88
EzeGomez0407 <gomezmerloe@gmail.com>
2025-06-07 18:24:12 -0300

    Funcionalidad de eliminar obra completa

---
+++ src/Components/ModalConfirmDeletWork.js
@@ -0,0 +1,61 @@
+import "./styles/ModalDelWork.css"
+import { useState } from "react"
+
+export default function ModalConfirmDeleteWork({work_name, work_id, closeModal}){
+    const [confirmName, setConfirmName] = useState("")
+    const [msg, setMsg] = useState("")
+
+    const handleChange = (e)=>{
+        const {value} = e.target
+        setConfirmName(value)
+    }
+
+    const handleClick = async(e)=>{
+        e.preventDefault()
+
+        if(confirmName === work_name){
+            
+            const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/delete-work`,{
+                method: "DELETE",
+                headers: {
+                "Content-Type": "application/json"
+                },
+                body: JSON.stringify({id: work_id})
+            })
+        
+            if(!res.ok) {
+                setMsg("Error de servidor")  
+                return console.log(res)
+            };
+        
+            const data = await res.json()
+            console.log(data);
+            window.location.reload()
+            return closeModal()
+        }else{
+            setMsg("No hay coincidencia")
+            return
+        }
+    }
+
+    return (
+        <div className="background_modaldelWork">
+            <div className="contain_form_delWork">
+                <p className="p-delConfirm">Debes confirmar la eliminación</p>
+                <form onChange={handleChange} className="form_delWork">
+                    <label htmlFor="input-confirmDelete">Escribe exactamente <span style={{color: 'red'}}>{work_name}</span></label>
+
+                    {
+                        msg.length > 0 && <p style={{fontSize: "1rem", textAlign: "center", color: "#e35752", fontWeight: '600'}}>{msg}</p>
+                    }
+
+                    <input type="text" id="input-confirmDelete"/>
+                    <div className="contain-btnModalDelet">
+                        <button onClick={closeModal} className="btns-del-modal btn-cancel">Cancelar</button>
+                        <button onClick={handleClick} className="btns-del-modal btn-del">Eliminar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
