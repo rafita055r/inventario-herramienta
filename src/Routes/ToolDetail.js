6807740d2a4f2d1f98b1dc5251d82953d15877bd
EzeGomez0407 <gomezmerloe@gmail.com>
2025-07-04 11:29:00 -0300

    componente para editar cantidad agregado'

--- src/Routes/ToolDetail.js
+++ src/Routes/ToolDetail.js
@@ -1,11 +1,31 @@
 import { useLocation, useNavigate, useParams } from "react-router"
 import "./styles/ToolDetail.css"
 import useStore from "../services/useStore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LoaderToData from "../Components/LoaderToData";
-import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 const urlImgGeneric = "https://static.vecteezy.com/system/resources/previews/009/098/401/large_2x/tool-icon-collection-illustration-instrument-symbol-wrench-hammer-handsaw-screwdriver-adjustment-wrench-paint-brush-vector.jpg"
+
+function useEditedTool(tool){
+    const [ toolEdited, setToolEdited ] = useState({
+        nombre: tool.nombre,
+        observacion: tool.observacion,
+        cantidad_total: tool.cantidad_total
+    })
+    const [msgError, setMsgError] = useState("")
+
+    const handleEditTool = (e)=>{
+        const {name, value} = e.target
+        console.log(name, value);
+        
+        if(name === "cantidad_total"){
+            // if(value < tool.can)
+        }
+        
+    }
+
+    return {toolEdited, handleEditTool, msgError}
+}
 
 export default function ToolDetail(){
     const {id} = useParams();
@@ -14,9 +34,9 @@ export default function ToolDetail(){
     const navigate = useNavigate()
     const {cargarDatosDesdeAPI} = useStore();
     const [delLoader, setDelLoader] = useState(false)
-    // const [editLoader, setEditLoader] = useState(false)
     const [isEditing, setIsEditing] = useState(false)
-    
+    const {toolEdited, handleEditTool, msgError} = useEditedTool(tool) 
+
     const onClickDelete = async ()=>{
         setDelLoader(true)
         try {
@@ -53,14 +73,19 @@ export default function ToolDetail(){
                             <button className="btn-del-tool" onClick={onClickDelete}>{delLoader ? <LoaderToData width={60} color={"#fefefe"} textOfCharging={"eliminando"}/> : "Eliminar"}</button>
                         </div>
                     }
-                    {isEditing ? <input type="text" placeholder={tool.nombre}/> : <h2>{tool.nombre}</h2>}
-                    {isEditing ? <textarea defaultValue={tool.observacion}/> : <p>{tool.observacion}</p>}
+                    {isEditing ? <input type="text" value={toolEdited.nombre} onChange={handleEditTool} name="nombre"/> : <h2>{tool.nombre}</h2>}
+                    {isEditing ? <textarea name="observacion" defaultValue={toolEdited.observacion} onChange={handleEditTool}/> : <p>{tool.observacion}</p>}
                 </div>
 
 
-                <div className="div-allQuantity-tool">
+                <div className={isEditing ? "div-allQuantity-tool editing" : "div-allQuantity-tool"}>
                     <h4>Cantidad total</h4>
-                    {isEditing ? <input type="number" placeholder={tool.cantidad_total}/> : <span style={{fontWeight: "600", color: "#16182d"}}>{tool.cantidad_total}</span>}
+                    {isEditing ? <>
+                        <EditedQuantityTool handlerEdit={handleEditTool} tool={tool}/>
+                        <p>Si le herramienta se encuentra en más de una obra no se puede restar la cantidad</p>
+                        {msgError && <p className="p-editTool-error">{msgError}</p>}
+                    </>
+                    : <span style={{fontWeight: "600", color: "#16182d"}}>{tool.cantidad_total}</span>}
                 </div>
                 <div className="div-table-ubication-quantity">
                     <div className="div-header-UbicQuant">
@@ -85,4 +110,56 @@ export default function ToolDetail(){
             </section>
         </main>
     )
+}
+
+function EditedQuantityTool ({handlerEdit, tool}){
+    const [count, setCount] = useState({
+        operation: "",
+        value: 0
+    })
+
+    const [result, setResult] = useState(tool.cantidad_total)
+
+    
+    const handledChange = (e)=>{
+        const value= parseInt(e.target.value)
+        const name = e.target.name
+
+        setCount({
+            operation: name,
+            value: isNaN(value) ? 0 : value
+        });
+    }
+
+    useEffect(()=>{
+
+        if(count.operation !== "" && count.value <= 0){
+            setCount({
+                operation: "",
+                value: 0
+            });
+        }
+
+        if(count.operation === "substract" && count.value > 0 && count.value < tool.cantidad_total) {
+            setResult((tool.cantidad_total - count.value))
+            handlerEdit({target:{value:count, name: "cantidad_total"}})
+        }
+        else if(count.operation === "addition" && count.value > 0) {
+            setResult((tool.cantidad_total + count.value))
+            handlerEdit({target:{value:count, name: "cantidad_total"}})
+        }
+        
+    },[count, handlerEdit, tool.cantidad_total])
+
+    return <div style={{display:"flex", gap: "20px", alignItems: "center"}}>
+        <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+            <label style={{fontSize:"1rem"}}>Quitar</label>
+            <input type="number" name="substract" onChange={handledChange} disabled={count.operation === "addition"} min={0} max={tool.cantidad_total - 1}/>
+        </div>
+        <p>{result}</p>
+        <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+            <label style={{fontSize:"1rem"}}>Agregar</label>
+            <input type="number" name="addition" onChange={handledChange} disabled={count.operation === "substract"} min={0}/>
+        </div>
+    </div>
 }
