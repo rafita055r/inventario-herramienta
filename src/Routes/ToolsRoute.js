d7335f5181d79d90600fe0c4a5dd808de355c8b7
EzeGomez0407 <gomezmerloe@gmail.com>
2026-01-26 19:26:28 -0300

    estilos del buscador de herramientas terminado

--- src/Routes/ToolsRoute.js
+++ src/Routes/ToolsRoute.js
@@ -4,35 +4,36 @@ import FormAddTool from '../Components/FormAddTool';
 import LoaderMain from "../Components/LoaderMain";
 import { useNavigate } from 'react-router';
 import useStore from '../services/useStore';
+import { IoMdSearch } from "react-icons/io";
 
-export default function ToolsRoute({toolsList}){
+export default function ToolsRoute({ toolsList }) {
     const navigate = useNavigate()
-    const {getToolByName} = useStore()
+    const { getToolByName } = useStore()
     const [showForm, setShowForm] = useState(false);
     const [loading, setLoading] = useState(false);
     const [notFoundTool, setNotFoundTool] = useState(false);
     const [resultSearch, setResultSearch] = useState([])
     const [dataSearching, setDataSearching] = useState("")
 
-    const toolDetailRedirectPage = (tool)=>{
-        navigate(`/herramientas/${tool.id}`, {state: {tool, page: "tool-detail"}})
+    const toolDetailRedirectPage = (tool) => {
+        navigate(`/herramientas/${tool.id}`, { state: { tool, page: "tool-detail" } })
     }
 
-    const handleShowForm = (is_Show)=>{
+    const handleShowForm = (is_Show) => {
         setShowForm(is_Show);
     }
 
-    const handleSearching = (e)=>{
-        const {value} = e.target
+    const handleSearching = (e) => {
+        const { value } = e.target
         setDataSearching(value)
     }
 
-    const onClickSearch = async(e)=>{
+    const onClickSearch = async (e) => {
         e.preventDefault()
         setLoading(true)
         const searchedResult = await getToolByName(dataSearching)
-        
-        if(!searchedResult.length) {
+
+        if (!searchedResult.length) {
             setResultSearch([])
             setNotFoundTool(true)
             return setLoading(false)
@@ -42,23 +43,26 @@ export default function ToolsRoute({toolsList}){
         return setLoading(false)
     }
 
-    useEffect(()=>{
-        if(dataSearching.trim() === "") setNotFoundTool(false)
-    },[])
-    return(
+    useEffect(() => {
+        if (dataSearching.trim() === "") {
+            setNotFoundTool(false)
+            setResultSearch([])
+        }
+    }, [dataSearching])
+    return (
         <main className='main-tool'>
 
             {showForm && (
-                <FormAddTool showForm={handleShowForm}/>
+                <FormAddTool showForm={handleShowForm} />
             )}
 
             <button className="add-tool-btn" onClick={() => setShowForm(!showForm)}>
                 {showForm ? 'Cancelar' : 'Agregar herramienta'}
             </button>
 
-            <div>
-                <input type="text" placeholder="Buscar herramientas" defaultValue={dataSearching} onChange={handleSearching} />
-                <button onClick={onClickSearch} >🔎</button>
+            <div className='contain-search-input' >
+                <input type="text" name='search-tool' className="input-search-tool" placeholder="Buscar herramientas" defaultValue={dataSearching} onChange={handleSearching} />
+                <button onClick={onClickSearch} className="btn-search-tool" ><IoMdSearch /></button>
             </div>
 
             <section className="sectionTools">
@@ -71,26 +75,26 @@ export default function ToolsRoute({toolsList}){
                     <span className="span-observation">Observación</span>
                 </div>
                 <ul className='ul_tools'>
-                    {   
-                        loading ? <LoaderMain/> :
-                        resultSearch.length && dataSearching.trim() !== '' ? resultSearch.map((tool)=> (
-                            <li key={tool.id} onClick={()=>toolDetailRedirectPage(tool)}>
-                                <p className="p-quantity">{tool.cantidad_total}</p>
-                                <p className="p-state">{tool.estado}</p>
-                                <p className="p-nameTools">{tool.nombre}</p>
-                                <p className="p-brand">{tool.marca}</p>
-                                <p className="p-measure">{tool.medidas}</p>
-                                <p className="p-observation">{tool.observacion}</p>
-                            </li>)) : !resultSearch.length && notFoundTool ? <span>No se encontraron herramientas con el nombre "{dataSearching}"</span> : toolsList.length > 0 ? toolsList.map((tool)=> (
-                            <li key={tool.id} onClick={()=>toolDetailRedirectPage(tool)}>
-                                <p className="p-quantity">{tool.cantidad_total}</p>
-                                <p className="p-state">{tool.estado}</p>
-                                <p className="p-nameTools">{tool.nombre}</p>
-                                <p className="p-brand">{tool.marca}</p>
-                                <p className="p-measure">{tool.medidas}</p>
-                                <p className="p-observation">{tool.observacion}</p>
-                            </li>
-                        )) : <LoaderMain/>
+                    {
+                        loading ? <LoaderMain /> :
+                            resultSearch.length && dataSearching.trim() !== '' ? resultSearch.map((tool) => (
+                                <li key={tool.id} onClick={() => toolDetailRedirectPage(tool)}>
+                                    <p className="p-quantity">{tool.cantidad_total}</p>
+                                    <p className="p-state">{tool.estado}</p>
+                                    <p className="p-nameTools">{tool.nombre}</p>
+                                    <p className="p-brand">{tool.marca}</p>
+                                    <p className="p-measure">{tool.medidas}</p>
+                                    <p className="p-observation">{tool.observacion}</p>
+                                </li>)) : !resultSearch.length && notFoundTool ? <span>No se encontraron herramientas con el nombre "{dataSearching}"</span> : toolsList.length > 0 ? toolsList.map((tool) => (
+                                    <li key={tool.id} onClick={() => toolDetailRedirectPage(tool)}>
+                                        <p className="p-quantity">{tool.cantidad_total}</p>
+                                        <p className="p-state">{tool.estado}</p>
+                                        <p className="p-nameTools">{tool.nombre}</p>
+                                        <p className="p-brand">{tool.marca}</p>
+                                        <p className="p-measure">{tool.medidas}</p>
+                                        <p className="p-observation">{tool.observacion}</p>
+                                    </li>
+                                )) : <LoaderMain />
                     }
                 </ul>
             </section>
